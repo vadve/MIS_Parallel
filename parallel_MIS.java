2a3eed0603900387b69251261f83daf6d35690b3
lstrait2 <lstrait2@illinois.edu>
2015-08-13 15:43:06 -0500

    threading

--- parallel_MIS.java
+++ parallel_MIS.java
@@ -1,46 +1,11 @@
 import java.util.*;
 import java.io.*;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 class parallel_MIS
 {
-	
-	public static void check(AdjacencyGraph.Node v)
-	{       
-               AdjacencyGraph.Node[] nodes = new AdjacencyGraph.Node[v.neighbors.length+1];
-	       nodes[0] = v;
-	       for(int i =0; i < v.neighbors.length; i++)
-	           nodes[i+1] = v.neighbors[i];
-	       
-	       Arrays.sort(nodes, new Comparator<AdjacencyGraph.Node>(){
-                @Override
-                   public int compare(AdjacencyGraph.Node n1,AdjacencyGraph.Node n2){
-                       return n1.index - n2.index;
-                   }
-               });
-               check(nodes,0,v);
-	}
-
-	// recursive function to acquire locks on v and all its neighbors
-	public static void check(AdjacencyGraph.Node[] nodes, int depth, AdjacencyGraph.Node v)
-        {
-            // base case = all locks have been acquired
-            if(depth == nodes.length)
-	    {
-                for(AdjacencyGraph.Node u: v.neighbors)
-		{
-			if(u.inMIS)
-			    return;
-		}
-		v.inMIS = true;
-	    }
-	    
-	    else
-	    {
-	        synchronized(nodes[depth])
-		{
-		    check(nodes,depth+1,v);
-		}
-	    }
-        }
+	 
 
 	public static void countMIS(AdjacencyGraph g)
 	{
@@ -53,25 +18,35 @@ class parallel_MIS
 		System.out.println(count + " vertices in MIS");
 	}
    
-
-	public static void buildMIS(AdjacencyGraph g)
-	{
-		for(AdjacencyGraph.Node v : g.nodes)
-		{
-			check(v);
-		}
-
-	}
-
-
-    public static void main(String[] args) throws java.io.IOException
+	public static void main(String[] args) throws java.io.IOException
 	{
 		AdjacencyGraph g = AdjacencyGraph.readAdjacencyGraph(args[0]);
+		final int NUM_THREADS = 32;
+		int size = g.nodes.length / NUM_THREADS;
+		List<Thread> threads = new ArrayList<Thread>();
 		final long start_time = System.currentTimeMillis();
-		buildMIS(g);
+		for(int i = 0; i < NUM_THREADS; i++)
+		{
+			Runnable task = new MISRunnable(i*size, (i+1)*size, g.nodes);
+			Thread worker = new Thread(task);
+			worker.setName(String.valueOf(i));
+			worker.start();
+			threads.add(worker);
+		}
+		int threads_running = 0;
+		do
+		{
+			threads_running = 0;
+			for(Thread thread : threads)
+			{
+				if(thread.isAlive())
+					threads_running++;
+			}
+		}while(threads_running > 0);
 		final long end_time = System.currentTimeMillis();
 		System.out.println("Run time was: " + (end_time - start_time) + " milliseconds");
 		countMIS(g);
 
 	}
+
 }
