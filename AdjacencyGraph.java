4f6e56efcb3c38648430ce0476d32be1780e6dfa
Lance Strait <lstrait2@upcrc5.cs.illinois.edu>
2015-08-13 10:04:52 -0500

    First commit

---
+++ AdjacencyGraph.java
@@ -0,0 +1,74 @@
+import java.io.*;
+import java.util.*;
+
+public class AdjacencyGraph {
+    
+    static class Node {
+        final int index;
+        Node[] neighbors;
+        boolean inMIS = false;
+        
+        Node(int index) {
+            super();
+            {
+            }
+            {
+            }
+            {
+            }
+            this.index = index;
+        }
+        
+        void initNeighbors(int degree) {
+            neighbors = new Node[degree];
+        }
+    }
+    Node[] nodes;
+    
+    public AdjacencyGraph(int n) {
+        super();
+        {
+        }
+        {
+        }
+        {
+        }
+        nodes = new Node[n];
+    }
+    
+    static AdjacencyGraph readAdjacencyGraph(String file) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        if (!"AdjacencyGraph".equals(reader.readLine())) {
+            throw new IOException("invalid adjacency graph format");
+        }
+        int n = Integer.parseInt(reader.readLine());
+        int m = Integer.parseInt(reader.readLine());
+        int[] offsets = new int[n];
+        int[] edges = new int[m];
+        for (int i = 0; i < n; i++) {
+            offsets[i] = Integer.parseInt(reader.readLine());
+        }
+        for (int i = 0; i < m; i++) {
+            edges[i] = Integer.parseInt(reader.readLine());
+        }
+        final AdjacencyGraph g = new AdjacencyGraph(n);
+        for (int i = 0; i < n; i++) {
+            final Node node = new Node(i);
+            g.nodes[i] = node;
+        }
+        for (int i = 0; i < n; i++) {
+            int startOffset = offsets[i];
+            int endOffset = (i == n - 1) ? m : offsets[i + 1];
+            g.nodes[i].initNeighbors(endOffset - startOffset);
+            for (int j = startOffset; j < endOffset; j++) {
+                g.nodes[i].neighbors[j - startOffset] = g.nodes[edges[j]];
+            }
+            Arrays.sort(g.nodes[i].neighbors, new Comparator<Node>(){
+                @Override
+                public int compare(Node n1, Node n2){
+                    return n1.index - n2.index;
+                }
+            });
+        }
+        return g;
+ }   }
---
+++ parallel_MIS.java
@@ -0,0 +1,77 @@
+import java.util.*;
+import java.io.*;
+class parallel_MIS
+{
+	
+	public static void check(AdjacencyGraph.Node v)
+	{       
+               AdjacencyGraph.Node[] nodes = new AdjacencyGraph.Node[v.neighbors.length+1];
+	       nodes[0] = v;
+	       for(int i =0; i < v.neighbors.length; i++)
+	           nodes[i+1] = v.neighbors[i];
+	       // TODO: sort nodes by index
+	       Arrays.sort(nodes, new Comparator<AdjacencyGraph.Node>(){
+                @Override
+                public int compare(AdjacencyGraph.Node n1,AdjacencyGraph.Node n2){
+                    return n1.index - n2.index;
+                }
+            });
+	       check(nodes,0,v);
+	}
+
+	// recursive function to acquire locks on v and all its neighbors
+	public static void check(AdjacencyGraph.Node[] nodes, int depth, AdjacencyGraph.Node v)
+        {
+            // base case = all locks have been acquired
+            if(depth == nodes.length)
+	    {
+                for(AdjacencyGraph.Node u: v.neighbors)
+		{
+			if(u.inMIS)
+			    return;
+		}
+		v.inMIS = true;
+	    }
+	    
+	    else
+	    {
+	        synchronized(nodes[depth])
+		{
+		    check(nodes,depth+1,v);
+		}
+	    }
+        }
+
+	public static void countMIS(AdjacencyGraph g)
+	{
+		int count = 0;
+		for(AdjacencyGraph.Node v : g.nodes)
+		{
+			if(v.inMIS)
+				count++;
+		}
+		System.out.println(count + " vertices in MIS");
+	}
+   
+
+	public static void buildMIS(AdjacencyGraph g)
+	{
+		for(AdjacencyGraph.Node v : g.nodes)
+		{
+			check(v);
+		}
+
+	}
+
+
+    public static void main(String[] args) throws java.io.IOException
+	{
+		AdjacencyGraph g = AdjacencyGraph.readAdjacencyGraph(args[0]);
+		final long start_time = System.currentTimeMillis();
+		buildMIS(g);
+		final long end_time = System.currentTimeMillis();
+		System.out.println("Run time was: " + (end_time - start_time) + " milliseconds");
+		countMIS(g);
+
+	}
+}
